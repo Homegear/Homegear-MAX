d34ba75ba68dd706e526a8711c26a2f84b9ef55a
Sathya Laufer <sathya@laufers.net>
2015-11-11 02:16:52 +0100

    Added base library version check

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -28,10 +28,16 @@
  */
 
 #include "Factory.h"
+#include "../config.h"
 
 BaseLib::Systems::DeviceFamily* MAXFactory::createDeviceFamily(BaseLib::Obj* bl, BaseLib::Systems::DeviceFamily::IFamilyEventSink* eventHandler)
 {
 	return new MAX::MAX(bl, eventHandler);
+}
+
+std::string getVersion()
+{
+	return VERSION;
 }
 
 BaseLib::Systems::SystemFactory* getFactory()
