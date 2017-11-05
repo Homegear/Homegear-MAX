11d6eae89183fc5960e36369d1385f84a21ff145
Sathya Laufer <sathya@laufers.net>
2017-11-05 13:32:53 +0100

    Left align type string in CLI

--- src/MAXCentral.cpp
+++ src/MAXCentral.cpp
@@ -1112,7 +1112,8 @@ std::string MAXCentral::handleCliCommand(std::string command)
 							typeID.resize(typeWidth2 - 3);
 							typeID += "...";
 						}
-						stringStream << std::setw(typeWidth2) << typeID << bar;
+						else typeID.resize(typeWidth2, ' ');
+						stringStream << typeID << bar;
 					}
 					else stringStream << std::setw(typeWidth2) << " " << bar;
 					if(i->second->getFirmwareVersion() == 0) stringStream << std::setfill(' ') << std::setw(firmwareWidth) << "?" << bar;
